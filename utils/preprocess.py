1432799f98885ebff2722b7c1de5f508b7c60a55
hikaru-nara <davidli@pku.edu.cn>
2020-02-14 21:40:02 -0800

    knn

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -82,14 +82,14 @@ class Preprocessor(object):
 			for i in range(batch_size):
 				time1 = time.time()
 				cpu_index = faiss.IndexFlatL2(3) # make 3 dim index
-				# gpu_index = faiss.index_cpu_to_all_gpus(  # build the index
-				    # cpu_index
-				# )
+				gpu_index = faiss.index_cpu_to_all_gpus(  # build the index
+				    cpu_index
+				)
 				print('batch_idx: ',i)
 				time2 = time.time()
-				cpu_index.add(tf.cast(xyz[i],tf.float32).eval(session=sess))
+				gpu_index.add(tf.cast(xyz[i],tf.float32).eval(session=sess))
 				time3 = time.time()
-				I,_ = cpu_index.search(tf.cast(new_xyz[i],tf.float32).eval(session=sess), K) # returns index and distance, I.shape = (npoint,K)
+				I,_ = gpu_index.search(tf.cast(new_xyz[i],tf.float32).eval(session=sess), K) # returns index and distance, I.shape = (npoint,K)
 				time4 = time.time()
 				idx_list.append(I)
 				print('time: ',time2-time1,time3-time2,time4-time3)
